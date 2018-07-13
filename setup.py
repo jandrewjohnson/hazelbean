1d0ff4c4a66a6cd523231da472029f1161ff50c6
Justin Johnson <jandrewjohnson@gmail.com>
2018-07-13 16:22:08 -0500

    hazelbean_0.6.6

--- setup.py
+++ setup.py
@@ -10,10 +10,9 @@ include_package_data=True
 setup(
   name = 'hazelbean',
   packages = packages,
-  version = '0.6.5',
+  version = '0.6.6',
   description = 'Geospatial research tools',
   long_description=long_description,
-  long_description_content_type="text/markdown",
   author = 'Justin Andrew Johnson',
   url = 'https://github.com/jandrewjohnson/hazelbean',
   download_url = 'https://github.com/jandrewjohnson/hazelbean',
