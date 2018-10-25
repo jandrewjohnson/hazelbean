fbeb9846c502424452e75ad8e75fbdb82e099763
Justin Johnson <jandrewjohnson@gmail.com>
2018-10-25 12:57:13 -0500

    hazelbean_0.7.9

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 
 from setuptools import setup, find_packages
 from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -11,7 +15,7 @@ include_package_data=True
 setup(
   name = 'hazelbean',
   packages = packages,
-  version = '0.7.5',
+  version = '0.7.9',
   description = 'Geospatial research tools',
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -20,7 +24,8 @@ setup(
   download_url = 'https://github.com/jandrewjohnson/hazelbean',
   keywords = ['geospatial', 'raster', 'shapefile', 'sustainability science'],
   classifiers = ["Programming Language :: Python :: 3"],
-  ext_modules=[Extension("cython_functions", ["cython_functions.c"]),
-               Extension("aspect_ratio_array_functions", ["aspect_ratio_array_functions.c"]),
-               ]
+  #cmdclass={'build_ext': build_ext},
+  #ext_modules=[Extension("cython_functions", ["hazelbean/calculation_core/cython_functions.c"]),
+  #             Extension("aspect_ratio_array_functions", ["hazelbean/calculation_core/aspect_ratio_array_functions.c"]),
+  #             ]
 )
